6976ff3b5522ae83876e872f61edfaefcd4ccd6a
四无浪子 <i@deonwu84.com>
2019-04-25 08:25:06 +0800

    初始化02

---
+++ 02_Logistic_Regression/costFunction.m
@@ -0,0 +1,11 @@
+function [J, grad] = costFunction(theta, X, y)
+
+m = length(y);
+J = 0;
+grad = zeros(size(theta));
+
+J = 1 / m * (- y' * log(sigmoid(X * theta)) - (1-y)' * (log(1 - sigmoid(X * theta))));
+
+grad = 1 / m * X' * (sigmoid(X * theta) - y);
+
+end
